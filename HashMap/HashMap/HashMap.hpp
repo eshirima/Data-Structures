34493c7d0c5fa767f6f368dee174dc8433c1209a
Emil Shirima <emil.shirima@hotmail.com>
2015-11-01 02:18:30 -0500

    Copied the cpp and header to the HashMap folder

---
+++ HashMap/HashMap/HashMap.hpp
@@ -0,0 +1,47 @@
+//
+//  HashMap.hpp
+//  HashMap
+//
+//  Created by Emil Shirima on 10/31/15.
+//  Copyright © 2015 Emil Shirima. All rights reserved.
+//
+
+#ifndef HashMap_hpp
+#define HashMap_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+class Item
+{
+public:
+    
+    Item();
+    Item(std::string);
+    std::string data;
+    Item * next;
+};
+
+class HashMap
+{
+public:
+    
+    HashMap();
+    int hash(std::string);
+    bool isIndexEmpty(int);
+    bool isBucketEmpty(int);
+    void addItem(std::string);
+    int numberOfElementsInBucket(int);
+    void printBucketsAtIndex(int);
+    void printEntireMap();
+    void printFilledAreasOfTheMap();
+    void search(std::string);
+    void remove(std::string);
+    
+private:
+    
+    static const int size = 100;
+    Item * hashTable[size];
+};
+
+#endif /* HashMap_hpp */
